316f2158910ef37508528142b1ac34a0f64465e1
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-06-01 16:33:15 +1000

    doing sample exam

--- src/comp1110/exam/Q4ArrayList.java
+++ src/comp1110/exam/Q4ArrayList.java
@@ -16,6 +16,15 @@ public class Q4ArrayList<T> {
      * @param value The value to be added.
      */
     public void add(T value) {
+        if ((elements + 1) >= values.length) {
+            T[] newValues = (T[]) new Object[(int) (values.length * GROWTH_FACTOR)];
+            for (int i = 0; i < elements; i++) {
+                newValues[i] = values[i];
+            }
+            values = newValues;
+        }
+        values[elements] = value;
+        elements = elements + 1;
         /* Unimplemented.  Q4 i) [7 Marks] */
     }
 
@@ -25,6 +34,21 @@ public class Q4ArrayList<T> {
      * @param index
      */
     public void remove(int index) {
+        if ((elements - 1) < (int) (values.length / GROWTH_FACTOR)) {
+            T[] newValues = (T[]) new Object[(int) (values.length / GROWTH_FACTOR)];
+
+            int j = 0;
+            for (int i = 0; i < elements; i++) {
+                if (i == index) continue;
+                newValues[j++] = values[i];
+            }
+            values = newValues;
+        } else {
+            for (int i = index; i < (elements - 1); i++) {
+                values[i] = values[i + 1];
+            }
+        }
+        elements--;
         /* Unimplemented. Q4 ii) [7 Marks] */
     }
 
@@ -49,7 +73,12 @@ public class Q4ArrayList<T> {
      * Reverse the order of the elements of the list.
      */
     public void reverse() {
-	    /* Unimplemented. Q4 iii) [6 Marks] */
+        for (int i = 0; i < elements / 2; i++) {
+            T temp = values[i];
+            values[i] = values[elements - i - 1];
+            values[elements - i - 1] = temp;
+        }
+        /* Unimplemented. Q4 iii) [6 Marks] */
     }
 
     /**
